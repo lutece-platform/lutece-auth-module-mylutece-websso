72fcfcf0fb6d03fdc1a2138658f85917598b955a
AnasBenjelloun <anasbenjellountouimi@gmail.com>
2026-04-01 17:22:32 +0200

    LUT-32204 Upgrade to commons-lang3

--- src/java/fr/paris/lutece/plugins/mylutece/modules/wssodatabase/authentication/service/WssoDatabaseService.java
+++ src/java/fr/paris/lutece/plugins/mylutece/modules/wssodatabase/authentication/service/WssoDatabaseService.java
@@ -51,7 +51,7 @@ import java.util.List;
 import java.util.Locale;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 
 /**
